83a922ea89bc7e297966dab279dca6a130cefdce
Jo Vinkenroye <jestersimpps@gmail.com>
2024-11-12 19:31:15 +0100

    chore: Remove commented-out boxShadow styles from Icon component

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -14,14 +14,14 @@ export const Icon: FC<IconProps> = ({ name }) => {
              hsl(0, 0%, 85%), 
              hsl(0, 0%, 95%), 
              hsl(0, 0%, 80%))`,
-           boxShadow: `
-             0 15px 25px -4px rgba(0, 0, 0, 0.3),
-             0 8px 12px -3px rgba(0, 0, 0, 0.2),
-             inset 0 -4px 8px rgba(0, 0, 0, 0.2),
-             inset 0 4px 8px rgba(255, 255, 255, 0.9),
-             inset -4px 0 8px rgba(0, 0, 0, 0.15),
-             inset 4px 0 8px rgba(255, 255, 255, 0.7)
-           `
+          //  boxShadow: `
+          //    0 15px 25px -4px rgba(0, 0, 0, 0.3),
+          //    0 8px 12px -3px rgba(0, 0, 0, 0.2),
+          //    inset 0 -4px 8px rgba(0, 0, 0, 0.2),
+          //    inset 0 4px 8px rgba(255, 255, 255, 0.9),
+          //    inset -4px 0 8px rgba(0, 0, 0, 0.15),
+          //    inset 4px 0 8px rgba(255, 255, 255, 0.7)
+          //  `
          }}>
       {name}
     </div>
