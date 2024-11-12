72d0b150e52ee69415eee9cf26cac16e97d4f788
Jo Vinkenroye <jestersimpps@gmail.com>
2024-11-12 20:00:57 +0100

    style: Remove redundant 'rounded-sm' class from Icon component

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -10,7 +10,7 @@ interface IconProps {
 export const Icon: FC<IconProps> = ({ name, style }) => {
  return (
   <div
-   className="flex items-center justify-center text-4xl w-full h-full rounded-sm shadow-lg transform transition-all hover:scale-105 hover:-scale cursor-pointer m-0 p-0"
+   className="flex items-center justify-center text-4xl w-full h-full shadow-lg transform transition-all hover:scale-105 hover:-scale cursor-pointer m-0 p-0"
    style={{
     ...style,
    }}
