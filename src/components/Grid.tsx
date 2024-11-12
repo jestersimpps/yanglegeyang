12b89880060bebc8cc0543bdb4430caf95f29911
Jo Vinkenroye <jestersimpps@gmail.com>
2024-11-12 17:05:36 +0100

    fix: Update top layer size to match first layer dimensions (360px)

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -67,7 +67,7 @@ const Grid: FC = () => {
       </div>
 
       {/* Top layer - 6x6 grid */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[240px] h-[240px] grid grid-cols-6 gap-0">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[360px] h-[360px] grid grid-cols-6 gap-0">
         {topCells.map((icon, index) => (
           <div 
             key={`top-${index}`}
